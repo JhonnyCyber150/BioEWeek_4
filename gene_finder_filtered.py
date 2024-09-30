cab8627034106c28677a46100e224a6eeb06f643
Jhonny Caicho <caichoj@login509-02-r.ibex.kaust.edu.sa>
2024-09-30 21:20:51 +0300

    add gene_finder_filtered.py

--- gene_finder_filtered.py
+++ gene_finder_filtered.py
@@ -2,35 +2,19 @@ import argparse
 from Bio import SeqIO
 from Bio.Seq import Seq
 
-def find_orfs(sequence, min_length):
-    orfs = []
-    seq_len = len(sequence)
-
-    for strand, seq in [(+1, sequence), (-1, sequence.reverse_complement())]:
-        for frame in range(3):
-            for start in range(frame, seq_len, 3):
-                if seq[start:start+3] == 'ATG':
-                    for end in range(start + 3, seq_len, 3):
-                        if seq[end:end+3] in ['TAA', 'TAG', 'TGA']:
-                            orf = seq[start:end+3]
-                            if len(orf) / 3 >= min_length:  # Check if ORF meets minimum length
-                                orfs.append((start, end+3, strand, orf))
-                            break
-    return orfs
 
 def main(input_file, min_length):
     for record in SeqIO.parse(input_file, "fasta"):
         sequence = record.seq
-        if len(sequence) < min_length:
+        if len(sequence) < min_length*3:
             continue
 
-        #orfs = find_orfs(sequence, min_length)
+        
         print(f">ORF")
         print(sequence)
-       # for start, end, strand, orf in orfs:
-        #    strand_symbol = '+' if strand == 1 else '-'
-        #    print(f">ORF_{start}_{end}_{strand_symbol}")
-        #    print(orf)
+       
+        
+       
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Find ORFs in a FASTA file")
